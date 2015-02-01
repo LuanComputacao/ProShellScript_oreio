7b6a46b63c5e363472936969a6fa8120723789e4
luancomputacao <luanengcomputacao@gmail.com>
2015-02-01 14:00:21 -0200

    Adiciona suporte à opção -V e opções inválidas

--- usuarios.sh
+++ usuarios.sh
@@ -4,33 +4,32 @@
 # Mostra os logins e nomes de usuários do sistema
 # Obs: Lê dados do arquivo /etc/passwd
 #
-# Aurelio, Novembro de 2007
-#
-# Versão 1: Mostra usuários e nomes separados por TAB
-# Versão 2: Adicionado suporte à opção -h
-#
-#
-#
 
 MENSAGEM_USO="
 Uso $0 [-h]
 
 
 -h              Mostra esta tela de ajuda e sai
+-V              Mostra a versão do programa e sai
 -e              Exporta o conteudo para um arquivo CSV
 "
+VERSAO="3"
 # Tratamento das opções de linha de comando
 
-if test "$1" = "-h"
-then
+case "$1" in
+-h)
     echo "$MENSAGEM_USO"
     exit 0
-elif test "$1" = "-V"
+;;
+-V)
+    echo $0 Versão $VERSAO
+    exit 0
+;;
+*)
+    echo "Oção invalida"
+    exit 1
 
-fi
-# if test "$1" = "-e"
 #     echo "login,usuario" > usuarios.csv
 #     cut -d : -f 1,5 /etc/passwd | tr -d , | tr : , >> usuarios.csv
-# fi
-
+esac
 cut -d : -f 1,5 /etc/passwd | tr -d , | tr : \\t
