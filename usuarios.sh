52b63b99b6f8573404f29cba696c922d9485a4d8
luancomputacao <luanengcomputacao@gmail.com>
2015-02-01 15:28:10 -0200

    Adiciona a opção -s e --sort

--- usuarios.sh
+++ usuarios.sh
@@ -11,7 +11,7 @@
 # Versão 4: Arrumando bug quando não tem opções, basename
 #           no nome do programa, -V extraindo direto dos cabeçalhos,
 #           adicionadas opções --help e --version
-#
+# Versão 5: Adiciona a opção -s e --sort
 #
 #
 #
@@ -19,33 +19,50 @@
 MENSAGEM_USO="
 Uso: $(basename $0) [-h | -V]
 
-
+-s, --sort          Ordena a listagem alfabeticamente
 -h, --help          Mostra esta tela de ajuda e sai
 -V, --version       Mostra a versão do programa e sai
 -e                  Exporta o conteudo para um arquivo CSV
 "
 VERSAO="3"
+ordenar=0
 # Tratamento das opções de linha de comando
 
 case "$1" in
--h | --help)
-    echo "$MENSAGEM_USO"
-    exit 0
-;;
--V | --version)
-    echo -n $(basename $0)
-    # Extrai a versão diretamente dos cabeçalhos do programa
-    grep '^# Versão ' usuarios.sh | tail -1 | cut -d : -f 1 | tr -d '# '
-    exit 0
-;;
-*)
-    if test -n "$1"
-    then
-        echo "Oção invalida"
-        exit 1
-    fi
-;;
+    -h | --help)
+        echo "$MENSAGEM_USO"
+        exit 0
+    ;;
+
+    -V | --version)
+        echo -n $(basename $0)
+        # Extrai a versão diretamente dos cabeçalhos do programa
+        grep '^# Versão ' usuarios.sh | tail -1 | cut -d : -f 1 | tr -d '# '
+        exit 0
+    ;;
+
+    -s | --sort)
+        ordenar=1
+    ;;
+
+    *)
+        if test -n "$1"
+        then
+            echo "Oção invalida"
+            exit 1
+        fi
+    ;;
 #     echo "login,usuario" > usuarios.csv
 #     cut -d : -f 1,5 /etc/passwd | tr -d , | tr : , >> usuarios.csv
 esac
-cut -d : -f 1,5 /etc/passwd | tr -d , | tr : \\t
+
+#Extrai a listagem
+lista=$(cut -d : -f 1,5 /etc/passwd | tr -d , | tr : \\t)
+
+#Ordena a listagem (se necessário)
+if test "$ordenar" = 1
+then
+    lista=$(echo "$lista" | sort)
+fi
+
+echo "$lista"
